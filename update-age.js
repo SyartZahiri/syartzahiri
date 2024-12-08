4b76fd7194e44d54f9f6226e0087625f4a32abc9
Syart Zahiri <5716549+SyartZahiri@users.noreply.github.com>
2024-12-08 17:04:46 +0100

    Update update-age.js

--- update-age.js
+++ update-age.js
@@ -16,13 +16,12 @@ function getAge(birthYear, birthMonth, birthDay) {
 }
 
 function updateReadme() {
-    const age = getAge(2009, 12, 31); // Beispiel: 31. Dezember 2009
-    const readmePath = path.join(__dirname, '../README.md');
+    const age = getAge(2009, 11, 31); // Geburtstag: 31. Dezember 2009
+    const readmePath = path.join(__dirname, 'README.md');
 
     let readmeContent = fs.readFileSync(readmePath, 'utf-8');
     const ageText = `Hello, I'm ${age} years old!`;
 
-    // Suche nach der Zeile und ersetze sie oder füge sie hinzu
     const ageLineRegex = /Hello, I'm \d+ years old!/;
     if (ageLineRegex.test(readmeContent)) {
         readmeContent = readmeContent.replace(ageLineRegex, ageText);
